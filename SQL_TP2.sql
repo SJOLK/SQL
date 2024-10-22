3df8efd0e2388530db8c241b130f394881b9b17a
SJOLK <123592790+SJOLK@users.noreply.github.com>
2024-10-22 14:07:57 +0200

    Update SQL_TP2.sql

--- SQL_TP2.sql
+++ SQL_TP2.sql
@@ -1,9 +1,7 @@
 
 MariaDB [(none)]> create database compta character set 'utf8' collate 'utf8_general_ci';
-Query OK, 1 row affected (1.827 sec)
 
 MariaDB [(none)]> use compta;
-Database changed
 
 
 MariaDB [compta]> create table article(
@@ -13,13 +11,11 @@ MariaDB [compta]> create table article(
     -> RRIX Decimal(6,2),
     -> ID_FOU int(6)
     -> );
-Query OK, 0 rows affected (2.362 sec)
 
 MariaDB [compta]> create table fournisseur(
     -> ID int(6) AUTO_INCREMENT PRIMARY KEY,
     -> NOM VARCHAR(30) NOT NULL
     -> );
-Query OK, 0 rows affected (2.378 sec)
 
 MariaDB [compta]> create table bon(
     -> ID int(6) AUTO_INCREMENT PRIMARY KEY,
@@ -28,7 +24,6 @@ MariaDB [compta]> create table bon(
     -> DELAI int,
     -> ID_FOU int(6)
     -> );
-Query OK, 0 rows affected (2.240 sec)
 
 MariaDB [compta]> create table compo(
     -> ID int(6) AUTO_INCREMENT PRIMARY KEY,
@@ -36,4 +31,3 @@ MariaDB [compta]> create table compo(
     -> ID_ART int(6),
     -> ID_BON int(6)
     -> );
-Query OK, 0 rows affected (2.356 sec)
